023afe99aa6ca66c9b0e83b5d62dc9e46cc4d42b
Illikan <39518414+Illikan@users.noreply.github.com>
2024-05-23 13:29:18 +0800

    Add files via upload

--- calculate_test.py
+++ calculate_test.py
@@ -1,7 +1,7 @@
 import unittest
 from tkinter import * # Import the essential modules for the graphical user interface.
 from tkinter import ttk
-from refactor_3 import *
+from calculate import calculate
 class calculate_test(unittest.TestCase):
      
    def test_sigleton(self):
@@ -10,3 +10,40 @@ class calculate_test(unittest.TestCase):
       rt1 = Tk()
       # Проверка
       assert(rt is not rt1)
+      
+   def test_addition(self):
+      # Подготовка
+      num1 = -2
+      num2 = -3
+      # Действие
+      result = calculate.addition(num1, num2)
+      # Проверка
+      assert(result == -5)
+      
+   def test_substraction(self):
+      # Подготовка
+      num1 = -2
+      num2 = -3
+      # Действие
+      result = calculate.substraction(num1, num2)
+      # Проверка
+      assert(result == 1)
+      
+   def test_multiplication(self):
+      # Подготовка
+      num1 = -2
+      num2 = -3
+      # Действие
+      result = calculate.multiplication(num1, num2)
+      # Проверка
+      assert(result == 6)
+      
+   def test_division(self):
+      # Подготовка
+      num1 = -2
+      num2 = -3
+      # Действие
+      result = calculate.division(num1, num2)
+      # Проверка
+      assert(result == 0.667)
+      
